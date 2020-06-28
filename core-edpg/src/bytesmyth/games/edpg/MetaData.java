fbd32b0f304bb2678a1225eb04443753164be209
Phrongorre <34555024+Phrongorre@users.noreply.github.com>
2020-06-28 14:10:32 -0700

    Initial setup

---
+++ core-edpg/src/bytesmyth/games/edpg/MetaData.java
@@ -0,0 +1,9 @@
+package bytesmyth.games.edpg;
+
+public class MetaData {
+
+	public static final float VIRTUAL_WIDTH = 800f;
+	public static final float VIRTUAL_HEIGHT = 600f;
+	public static final boolean RESIZEABLE = false;
+
+}
--- desktop-edpg/src/bytesmyth/games/edpg/desktop/DesktopLauncher.java
+++ desktop-edpg/src/bytesmyth/games/edpg/desktop/DesktopLauncher.java
@@ -2,11 +2,18 @@ package bytesmyth.games.edpg.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import bytesmyth.games.edpg.ExecDysfuncPlatformer;
+import bytesmyth.games.edpg.MetaData;
 
 public class DesktopLauncher {
+	
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = (int)MetaData.VIRTUAL_WIDTH;
+		config.height = (int)MetaData.VIRTUAL_HEIGHT;
+		config.resizable = MetaData.RESIZEABLE;
 		new LwjglApplication(new ExecDysfuncPlatformer(), config);
 	}
+	
 }
