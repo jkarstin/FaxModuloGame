caed0d5834579cb16fae965aabd10f78f1871eb7
Phrongorre <34555024+Phrongorre@users.noreply.github.com>
2020-07-01 22:35:29 -0700

    Imported (copied) Collidable object from Wishing Well game

--- core-edpg/src/bytesmyth/games/edpg/util/MetaData.java
+++ core-edpg/src/bytesmyth/games/edpg/util/MetaData.java
@@ -5,5 +5,6 @@ public class MetaData {
 	public static final float VIRTUAL_WIDTH = 800f;
 	public static final float VIRTUAL_HEIGHT = 600f;
 	public static final boolean RESIZEABLE = false;
+	public static boolean SHOW_WIREFRAMES = false;
 
 }
