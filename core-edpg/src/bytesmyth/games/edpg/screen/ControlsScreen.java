1db3aa5984bd597702a49539c7d0c3d059c87596
Phrongorre <34555024+Phrongorre@users.noreply.github.com>
2020-07-24 14:57:21 -0700

    Updated Controls Screen

--- core-edpg/src/bytesmyth/games/edpg/screen/ControlsScreen.java
+++ core-edpg/src/bytesmyth/games/edpg/screen/ControlsScreen.java
@@ -6,10 +6,12 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
 
 import bytesmyth.games.edpg.ExecDysfuncPlatformer;
 import bytesmyth.games.edpg.actor.GameActor;
 import bytesmyth.games.edpg.util.Assets;
+import bytesmyth.games.edpg.util.MetaData;
 
 public class ControlsScreen extends BasicScreen {
 	
@@ -20,6 +22,8 @@ public class ControlsScreen extends BasicScreen {
 		(new GameActor(this.stage)).loadTexture("controls.png");
 		
 		this.backButton = new TextButton("Back", Assets.skin);
+		this.backButton.setWidth(80f);
+		this.backButton.setPosition(MetaData.VIRTUAL_WIDTH-40f, 40f, Align.bottomRight);
 		this.backButton.addListener(new ClickListener() {
 			
 			@Override
